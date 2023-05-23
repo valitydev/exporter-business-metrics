db6bc1bd6afc7a840d1b80011eb1ff7530120803
Anatoly Karlov <karleowne@gmail.com>
2023-05-23 16:02:16 +0600

    BUS-18: init flow

---
+++ postgres/terminal.sql
@@ -0,0 +1,6 @@
+insert into dw.terminal (id, version_id, terminal_ref_id, name, description, risk_coverage, terms_json, wtime, current, external_terminal_id, external_merchant_id, mcc, terminal_provider_ref_id)
+values  (3939938000006312765, 6805889491255870917, 917338300, 'emLuiRK', 'xuZxPyemtMsE', 'QSDBtDzNLAHGCsb', 'hCiTTBTeNytBijdyKGWtEQDn', '2014-04-10 05:07:33.000000', false, 'mnrc', 'dqJLATeqW', 'lXrojoOJRbcwjLqkllAsmYCCU', 2138075965),
+        (1, 6805889491255870917, 1, 'mts rub', 'xuZxPyemtMsE', 'QSDBtDzNLAHGCsb', 'hCiTTBTeNytBijdyKGWtEQDn', '2014-04-10 05:07:33.000000', true, 'mnrc', 'dqJLATeqW', 'lXrojoOJRbcwjLqkllAsmYCCU', 2138075965),
+        (2, 6805889491255870917, 2, 'xxx usd', 'xuZxPyemtMsE', 'QSDBtDzNLAHGCsb', 'hCiTTBTeNytBijdyKGWtEQDn', '2014-04-10 05:07:33.000000', true, 'mnrc', 'dqJLATeqW', 'lXrojoOJRbcwjLqkllAsmYCCU', 2138075965),
+        (3, 6805889491255870917, 3, 'reppay kzt', 'xuZxPyemtMsE', 'QSDBtDzNLAHGCsb', 'hCiTTBTeNytBijdyKGWtEQDn', '2014-04-10 05:07:33.000000', true, 'mnrc', 'dqJLATeqW', 'lXrojoOJRbcwjLqkllAsmYCCU', 2138075965),
+        (4, 6805889491255870917, 4, 'tinfokk uah', 'xuZxPyemtMsE', 'QSDBtDzNLAHGCsb', 'hCiTTBTeNytBijdyKGWtEQDn', '2014-04-10 05:07:33.000000', true, 'mnrc', 'dqJLATeqW', 'lXrojoOJRbcwjLqkllAsmYCCU', 2138075965);
