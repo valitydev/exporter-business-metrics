17bf06afa8e02724f97c74b5618f7fb42495707f
Anatoly Karlov <karleowne@gmail.com>
2023-05-23 18:59:18 +0600

    BUS-18: init flow

--- src/main/java/dev/vality/businessmetricexporter/entity/PaymentDto.java
+++ src/main/java/dev/vality/businessmetricexporter/entity/PaymentDto.java
@@ -1,44 +1,100 @@
 package dev.vality.businessmetricexporter.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
-@Builder
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
+@NamedNativeQuery(
+        name = "get_payment_dto_list",
+        query = """
+                with p4 as (with p3 as (with p2 as (with p1 as (select p.invoice_id,
+                                                                       p.payment_id,
+                                                                       p.currency_code,
+                                                                       p.event_created_at,
+                                                                       s.status
+                                                                from dw.payment as p
+                                                                         inner join dw.payment_status_info as s
+                                                                                    on p.invoice_id = s.invoice_id and
+                                                                                       p.payment_id = s.payment_id and s.current
+                                                                where p.event_created_at > now() - interval '?1 second'
+                                                                order by p.event_created_at desc)
+                                                    select p1.*,
+                                                           c.issuer_country as country
+                                                    from p1
+                                                             left join dw.payment_payer_info as c
+                                                                       on p1.invoice_id = c.invoice_id and p1.payment_id = c.payment_id)
+                                        select p2.*,
+                                               pt.route_provider_id as provider_id,
+                                               pt.route_terminal_id as terminal_id
+                                        from p2
+                                                 inner join dw.payment_route as pt
+                                                            on p2.invoice_id = pt.invoice_id and p2.payment_id = pt.payment_id and
+                                                               pt.current)
+                            select p3.*,
+                                   p.name as provider_name
+                            from p3
+                                     inner join dw.provider as p
+                                                on p3.provider_id = p.provider_ref_id and p.current)
+                select p4.invoice_id       as invoiceId,
+                       p4.payment_id       as paymentId,
+                       p4.currency_code    as currencyCode,
+                       p4.event_created_at as createdAt,
+                       p4.status           as status,
+                       p4.country          as country,
+                       p4.provider_id      as providerId,
+                       p4.terminal_id      as terminalId,
+                       p4.provider_name    as providerName,
+                       t.name              as terminalName
+                from p4
+                         inner join dw.terminal as t
+                                    on p4.terminal_id = t.terminal_ref_id and t.current
+                """,
+        resultSetMapping = "payment_dto_list")
+@SqlResultSetMapping(
+        name = "payment_dto_list",
+        classes = @ConstructorResult(
+                targetClass = PaymentDto.class,
+                columns = {
+                        @ColumnResult(name = "invoiceId", type = String.class),
+                        @ColumnResult(name = "paymentId", type = String.class),
+                        @ColumnResult(name = "status", type = String.class),
+                        @ColumnResult(name = "createdAt", type = String.class),
+                        @ColumnResult(name = "country", type = String.class),
+                        @ColumnResult(name = "currencyCode", type = String.class),
+                        @ColumnResult(name = "providerId", type = String.class),
+                        @ColumnResult(name = "terminalId", type = String.class),
+                        @ColumnResult(name = "providerName", type = String.class),
+                        @ColumnResult(name = "terminalName", type = String.class)}))
 public class PaymentDto {
 
     @Id
-    @GeneratedValue
     private Long id;
-    @Column(name = "invoice_id")
     private String invoiceId;
-    @Column(name = "payment_id")
     private String paymentId;
-    @Column(name = "status")
     private String status;
-    @Column(name = "event_created_at")
     private String createdAt;
-    @Column(name = "country")
     private String country;
-    @Column(name = "currency_code")
     private String currencyCode;
-    @Column(name = "provider_id")
     private String providerId;
-    @Column(name = "terminal_id")
     private String terminalId;
-    @Column(name = "provider_name")
     private String providerName;
-    @Column(name = "terminal_name")
     private String terminalName;
 
+    public PaymentDto() {
+    }
+
+    public PaymentDto(String invoiceId, String paymentId, String status, String createdAt, String country, String currencyCode, String providerId, String terminalId, String providerName, String terminalName) {
+        this.invoiceId = invoiceId;
+        this.paymentId = paymentId;
+        this.status = status;
+        this.createdAt = createdAt;
+        this.country = country;
+        this.currencyCode = currencyCode;
+        this.providerId = providerId;
+        this.terminalId = terminalId;
+        this.providerName = providerName;
+        this.terminalName = terminalName;
+    }
 }
--- src/main/java/dev/vality/businessmetricexporter/repository/PaymentRepository.java
+++ src/main/java/dev/vality/businessmetricexporter/repository/PaymentRepository.java
@@ -14,42 +14,7 @@ import java.util.List;
 @SuppressWarnings("LineLength")
 public interface PaymentRepository extends JpaRepository<PaymentEntity, PaymentPk> {
 
-    @Query(value = """
-            with p4 as (with p3 as (with p2 as (with p1 as (select p.invoice_id,
-                                                                   p.payment_id,
-                                                                   p.currency_code,
-                                                                   p.event_created_at,
-                                                                   s.status
-                                                            from dw.payment as p
-                                                                     inner join dw.payment_status_info as s
-                                                                                on p.invoice_id = s.invoice_id and
-                                                                                   p.payment_id = s.payment_id and s.current
-                                                            where p.event_created_at > now() - interval '?1 second'
-                                                            order by p.event_created_at desc)
-                                                select p1.*,
-                                                       c.issuer_country as country
-                                                from p1
-                                                         left join dw.payment_payer_info as c
-                                                                   on p1.invoice_id = c.invoice_id and p1.payment_id = c.payment_id)
-                                    select p2.*,
-                                           pt.route_provider_id as provider_id,
-                                           pt.route_terminal_id as terminal_id
-                                    from p2
-                                             inner join dw.payment_route as pt
-                                                        on p2.invoice_id = pt.invoice_id and p2.payment_id = pt.payment_id and
-                                                           pt.current)
-                        select p3.*,
-                               p.name as provider_name
-                        from p3
-                                 inner join dw.provider as p
-                                            on p3.provider_id = p.provider_ref_id and p.current)
-            select p4.*,
-                   t.name as terminal_name
-            from p4
-                     inner join dw.terminal as t
-                                on p4.terminal_id = t.terminal_ref_id and t.current
-                                """,
-            nativeQuery = true)
+    @Query(name = "get_payment_dto_list", nativeQuery = true)
     List<PaymentDto> getPaymentDtoList(String intervalTime);
 
 }
