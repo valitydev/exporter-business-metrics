2edce192d72e888265d53ad75bc6101f0fe1e629
Anatoly Karlov <karleowne@gmail.com>
2023-05-23 16:40:47 +0600

    BUS-18: init flow

--- src/main/java/dev/vality/businessmetricexporter/repository/PaymentRepository.java
+++ src/main/java/dev/vality/businessmetricexporter/repository/PaymentRepository.java
@@ -13,26 +13,42 @@ import java.util.List;
 @SuppressWarnings("LineLength")
 public interface PaymentRepository extends JpaRepository<PaymentRepository, PaymentPk>, JpaSpecificationExecutor<PaymentRepository> {
 
-    @Query(value = "with currppt as (with currp as ( " +
-            "select p.invoice_id, p.payment_id, s.status, p.event_created_at, pi.issuer_country as country, p.currency_code" +
-            "from dw.payment as p " +
-            "inner join dw.payment_status_info as s" +
-            "on p.invoice_id = s.invoice_id and p.payment_id = s.payment_id and s.current" +
-            "left join dw.payment_payer_info as pi" +
-            "on p.invoice_id = pi.invoice_id and p.payment_id = pi.payment_id" +
-            "where p.event_created_at > now() - interval '?1 second'" +
-            "order by p.event_created_at desc)" +
-            "select currp.invoice_id, currp.payment_id, currp.status, currp.event_created_at, currp.country, currp.currency_code, pt.route_provider_id as provider_id, pt.route_terminal_id as terminal_id " +
-            "from currp " +
-            "inner join dw.payment_route as pt" +
-            "on currp.invoice_id = pt.invoice_id and currp.payment_id = pt.payment_id and pt.current)" +
-            "select currppt.invoice_id, currppt.payment_id, currppt.status, currppt.event_created_at, currppt.country, currppt.currency_code, currppt.provider_id, currppt.terminal_id, p.name as provider_name, t.name as terminal_name " +
-            "from currppt" +
-            "inner join dw.provider as p" +
-            "on currppt.provider_id = t.provider_ref_id and p.current " +
-            "inner join dw.terminal as t" +
-            "on currppt.terminal_id = t.terminal_ref_id and t.current " +
-            "", nativeQuery = true)
+    @Query(value = """
+            with p4 as (with p3 as (with p2 as (with p1 as (select p.invoice_id,
+                                                                   p.payment_id,
+                                                                   p.currency_code,
+                                                                   p.event_created_at,
+                                                                   s.status
+                                                            from dw.payment as p
+                                                                     inner join dw.payment_status_info as s
+                                                                                on p.invoice_id = s.invoice_id and
+                                                                                   p.payment_id = s.payment_id and s.current
+                                                            where p.event_created_at > now() - interval '?1 second'
+                                                            order by p.event_created_at desc)
+                                                select p1.*,
+                                                       c.issuer_country as country
+                                                from p1
+                                                         left join dw.payment_payer_info as c
+                                                                   on p1.invoice_id = c.invoice_id and p1.payment_id = c.payment_id)
+                                    select p2.*,
+                                           pt.route_provider_id as provider_id,
+                                           pt.route_terminal_id as terminal_id
+                                    from p2
+                                             inner join dw.payment_route as pt
+                                                        on p2.invoice_id = pt.invoice_id and p2.payment_id = pt.payment_id and
+                                                           pt.current)
+                        select p3.*,
+                               p.name as provider_name
+                        from p3
+                                 inner join dw.provider as p
+                                            on p3.provider_id = p.provider_ref_id and p.current)
+            select p4.*,
+                   t.name as terminal_name
+            from p4
+                     inner join dw.terminal as t
+                                on p4.terminal_id = t.terminal_ref_id and t.current
+                                """,
+            nativeQuery = true)
     List<PaymentDto> getPaymentDtoList(String intervalTime);
 
 }
